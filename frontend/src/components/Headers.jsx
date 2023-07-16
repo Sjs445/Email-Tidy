0240797ef31de597da4bde81593f5c686a33cb54
Sjs445 <sjs445@csu.fullerton.edu>
2023-07-16 13:55:31 -0700

    add register, login, logout

--- frontend/src/components/Headers.jsx
+++ frontend/src/components/Headers.jsx
@@ -1,24 +1,46 @@
 import { FaSignInAlt, FaSignOutAlt, FaUser} from 'react-icons/fa'
-import { Link } from 'react-router-dom'
+import { Link, UseNavigate, useNavigate } from 'react-router-dom'
+import { UseSelector, useDispatch, useSelector } from 'react-redux';
+import {logout, reset} from '../features/auth/authSlice';
+
 
 
 function Headers() {
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const  { user } = useSelector( (state) => state.auth);
+
+    const onLogout = () => {
+        dispatch(logout());
+        dispatch(reset());
+        navigate('/');
+    }
+
   return (
     <header className='header'>
         <div className='logo'>
             <Link to='/'>EmailTidy</Link>
         </div>
         <ul>
-            <li>
-                <Link to='/login'>
-                    <FaSignInAlt /> Login
-                </Link>
+            {user ? (
+                <li>
+                <button className='btn' onClick={onLogout}>
+                    <FaSignOutAlt /> Logout
+                </button>
             </li>
+            ) : (<>
             <li>
-                <Link to='/register'>
-                    <FaUser /> Register
-                </Link>
-            </li>
+            <Link to='/login'>
+                <FaSignInAlt /> Login
+            </Link>
+        </li>
+        <li>
+            <Link to='/register'>
+                <FaUser /> Register
+            </Link>
+        </li>
+            </>) }
+            
         </ul>
     </header>
   )
